8b3eb15ab86f7c715c9811758555230283ae8f83
Shan Kariuki <shanicekariuki004@gmail.com>
2023-04-18 12:22:21 +0300

    queryset related_items

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=120)
+
+    class Meta:
+        ordering=['-name']
+        verbose_name_plural='Categories'
+
+    def __str__(self):
+        return self.name    
+    
+
+class Pic(models.Model):
+    category=models.ForeignKey(Category, related_name='pics', on_delete=models.SET_NULL, null=True)
+    name=models.CharField(max_length=50)
+    description=models.TextField(blank=True, null=True)
+    image=models.ImageField(upload_to='image')
+    created_by=models.ForeignKey(User,related_name='pics',on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return  self.name
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 
+from .models import *
 
+def index(request):
+    categories=Category.objects.all()
+    pics=Pic.objects.all()
+    context={'categories':categories,'pics':pics}
+    return render(request, 'photo/index.html', context )
+
+def view_photo(request, id):
+    pics=get_object_or_404(Pic,id=id)
+    related_items=Pic.objects.filter(category=pics.category).exclude(id=id)[0:3]
+    context={'pics':pics,
+             'related_items':related_items}
+    return render(request, 'photo/view.html', context)
