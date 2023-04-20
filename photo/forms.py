c6133df55b99d07168f5c3736e49796a72b29bdf
zablon <imjunior254@gmail.com>
2023-04-20 11:49:00 +0300

    styling detail page

--- photo/forms.py
+++ photo/forms.py
@@ -1,7 +1,23 @@
 from django.forms import ModelForm
 from .models import Pic
+from django import forms
 
-class PicForm(ModelForm):
+INPUT_CLASSES='w-full py-4 px-6 rounded-xl border'
+class PicForm(forms.ModelForm):
      class Meta:
         model=Pic
         fields=['category','name','description','image',]
+        widgets={
+             'category':forms.Select(attrs={
+              'class':INPUT_CLASSES
+       }),
+       'name':forms.TextInput(attrs={
+              'class':INPUT_CLASSES
+       }),
+       'description':forms.Textarea(attrs={
+              'class':INPUT_CLASSES
+       }),
+       'image':forms.FileInput(attrs={
+              'class':INPUT_CLASSES
+       }),
+        }
